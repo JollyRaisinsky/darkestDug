ed8b813ccd251972e11c131313d536a00d0cbcbd
Jack-Tadych <61060120+Jack-Tadych@users.noreply.github.com>
2023-04-29 20:19:13 -0400

    Update PlayerController.cs
    possible direction

--- PlayerController.cs
+++ PlayerController.cs
@@ -5,8 +5,22 @@ public class PlayerController : MonoBehaviour
     public float moveSpeed = 5f;
     public float attackRange = 2f;
     public int attackDamage = 10;
+    public int health = 100;
 
 
+    public void TakeDamage(int damage)
+    {
+        health -= damage;
+        print("Ouch! Health is now " + health);
+       
+        if (health <= 0)
+        {
+            // Die
+            print("You died!");
+            Destroy(gameObject);
+        }
+
+    }
 
     void Update()
     {
@@ -49,19 +63,46 @@ public class PlayerController : MonoBehaviour
         Collider[] hitColliders = Physics.OverlapSphere(transform.position, attackRange);
 
         // Loop through all colliders and apply damage to enemies
-       foreach (Collider enemy in hitColliders)
+       foreach (Collider hitCollider in hitColliders)
+        {
+            // Get the tag of the hit collider
+            string colliderTag = hitCollider.tag;
+
+            // Check if the collider belongs to an enemy
+            if (colliderTag == "enemy")
             {
-                if (enemy.CompareTag("enemy"))
-                {
-                    // Deal damage to the enemy
-                    //enemy.GetComponent<EnemyHealth>().TakeDamage(attackDamage);
-                    print("Enemy hit!");
-                }
-                print("Enemy not hit!");
+                // Deal damage to the enemy
+                //hitCollider.GetComponent<EnemyHealth>().TakeDamage(attackDamage);
+                print("Enemy hit!");
             }
+            else
+            {
+                print("Object with tag " + colliderTag + " hit!");
+            }
+        }
+
     }
 
-   
+   void OnDrawGizmosSelected()
+{
+    // Draw a wire sphere around the player to show the range of the attack
+    Gizmos.color = Color.red;
+    Gizmos.DrawWireSphere(transform.position, attackRange);
+
+    // Draw a sphere for each enemy within range
+    Collider[] hitColliders = Physics.OverlapSphere(transform.position, attackRange);
+    foreach (Collider hitCollider in hitColliders)
+    {
+        // Check if the collider belongs to an enemy
+        EnemyController enemy = hitCollider.GetComponent<EnemyController>();
+        if (enemy != null)
+        {
+            Gizmos.color = Color.yellow;
+            Gizmos.DrawSphere(enemy.transform.position, 0.5f);
+        }
+    }
+}
+
 }
 
 
